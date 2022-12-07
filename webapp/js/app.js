ff539bcf3bc54675b2c17aa3ec54a5d43746db70
Marc Kohaupt <debuglevel@gmail.com>
2022-12-07 16:04:09 +0100

    Support multiline input

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -1,6 +1,22 @@
 (function () {
   'use strict';
 
+  function getRemoteCouchDbUrl(password) {
+    let username = 'marc';
+    let host = 'debuglevel.de';
+    let path = 'couchdb/rapidjournal';
+    return 'https://' + username + ':' + password + '@' + host + '/' + path;
+  }
+
+  function promptPassword() {
+    let password = prompt("Please enter your password");
+    if (password == null || password == "") {
+      return undefined;
+    } else {
+      return password;
+    }
+  }
+
   var ENTER_KEY = 13;
   var newEntryDom = document.getElementById('new-entry');
   var syncDom = document.getElementById('sync-wrapper');
@@ -8,20 +24,21 @@
   // Use local database to write in offline mode and sync afterwards.
   var db = new PouchDB('rapidjournal');
 
-  let password = prompt("Please enter your password");
+  let password = promptPassword();
+
   // Remote database to replicate/sync to when online.
   let remoteCouch;
   if (password == null || password == "") {
     remoteCouch = undefined;
   } else {
-    remoteCouch = 'https://marc:'+password+'@debuglevel.de/couchdb/rapidjournal';
+    remoteCouch = getRemoteCouchDbUrl(password);
   }
 
   // Subscribe to database changes
   db.changes({
     since: 'now',
     live: true
-  }).on('change', showEntrys);
+  }).on('change', showAllEntries);
 
   /**
    * Adds an entry to the local database.
@@ -37,7 +54,7 @@
     db.put(entry, function callback(err, result) {
       if (!err) {
         console.log('Posting entry successful.');
-      }else{
+      } else {
         console.error('Posting entry failed.');
         console.error(err);
       }
@@ -47,9 +64,9 @@
   /**
    * Reads all entries from the local database and renders them.
    */
-  function showEntrys() {
+  function showAllEntries() {
     db.allDocs({ include_docs: true, descending: true }, function (err, result) {
-      renderEntrys(result.rows);
+      renderAllEntries(result.rows);
     });
   }
 
@@ -57,7 +74,7 @@
    * Removes an entry from the local database.
    * @param {*} entry The entry object to remove (probably only needs an object with _id set).
    */
-  function deleteButtonPressed(entry) {
+  function onDeleteButtonPressed(entry) {
     db.remove(entry);
   }
 
@@ -80,14 +97,14 @@
     syncDom.setAttribute('data-sync-state', 'syncing');
 
     var opts = { live: true };
-    db.replicate.to(remoteCouch, opts, syncError);
-    db.replicate.from(remoteCouch, opts, syncError);
+    db.replicate.to(remoteCouch, opts, showSynchronizationError);
+    db.replicate.from(remoteCouch, opts, showSynchronizationError);
   }
 
   /**
    * Show sync error on UI.
    */
-  function syncError() {
+  function showSynchronizationError() {
     syncDom.setAttribute('data-sync-state', 'error');
   }
 
@@ -95,7 +112,7 @@
    * Display input box to edit an entry.
    * @param {*} entry 
    */
-  function entryDoubleClicked(entry) {
+  function onEntryDoubleClicked(entry) {
     var div = document.getElementById('li_' + entry._id);
     var inputEditEntry = document.getElementById('input_' + entry._id);
     div.className = 'editing';
@@ -104,7 +121,7 @@
 
   // If they press enter while editing an entry, blur it to trigger save
   // TODO: Seems like a hack.
-  function entryKeyPressed(entry, event) {
+  function onEntryKeyPressed(entry, event) {
     if (event.keyCode === ENTER_KEY) {
       var inputEditEntry = document.getElementById('input_' + entry._id);
       inputEditEntry.blur();
@@ -121,15 +138,15 @@
     datelabel.className = 'datetime';
     datelabel.appendChild(document.createTextNode(
       new Date(entry.datetime).toLocaleDateString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' })
-      ));
+    ));
 
     var label = document.createElement('label');
     label.appendChild(document.createTextNode(entry.content));
-    label.addEventListener('dblclick', entryDoubleClicked.bind(this, entry));
+    label.addEventListener('dblclick', onEntryDoubleClicked.bind(this, entry));
 
     var deleteLink = document.createElement('button');
     deleteLink.className = 'delete';
-    deleteLink.addEventListener('click', deleteButtonPressed.bind(this, entry));
+    deleteLink.addEventListener('click', onDeleteButtonPressed.bind(this, entry));
 
     var divDisplay = document.createElement('div');
     divDisplay.className = 'view';
@@ -141,7 +158,7 @@
     inputEditEntry.id = 'input_' + entry._id;
     inputEditEntry.className = 'edit';
     inputEditEntry.value = entry.content;
-    inputEditEntry.addEventListener('keypress', entryKeyPressed.bind(this, entry));
+    inputEditEntry.addEventListener('keypress', onEntryKeyPressed.bind(this, entry));
     inputEditEntry.addEventListener('blur', entryBlurred.bind(this, entry));
 
     var li = document.createElement('li');
@@ -156,7 +173,7 @@
    * Renders all entries into the list.
    * @param {*} entrys 
    */
-  function renderEntrys(entrys) {
+  function renderAllEntries(entrys) {
     var ul = document.getElementById('entry-list');
 
     ul.innerHTML = '';
@@ -166,8 +183,12 @@
     });
   }
 
-  function newEntryKeyPressHandler(event) {
-    if (event.keyCode === ENTER_KEY) {
+  /**
+   * Adds a new entry when the user presses enter on the input field.
+   * @param {*} event 
+   */
+  function onNewEntryKeyPressed(event) {
+    if (!event.shiftKey && event.keyCode === ENTER_KEY) {
       addEntry(newEntryDom.value);
       newEntryDom.value = '';
     }
@@ -177,11 +198,11 @@
    * Adds event listeners to the new-entry input box.
    */
   function addEventListeners() {
-    newEntryDom.addEventListener('keypress', newEntryKeyPressHandler, false);
+    newEntryDom.addEventListener('keypress', onNewEntryKeyPressed, false);
   }
 
   addEventListeners();
-  showEntrys();
+  showAllEntries();
 
   if (remoteCouch) {
     initializeSynchronization();
